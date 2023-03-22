1ddc8790a7b49c1fa586335530a5dca3e79c08c5
Lucas pinheiro <lucaslogos@gmail.com>
2023-03-22 13:41:11 -0300

    update

--- Menu.cs
+++ Menu.cs
@@ -45,8 +45,17 @@ namespace Editor
             Console.SetCursorPosition(3, 2);
             Console.WriteLine("Editor HTML");
             Console.SetCursorPosition(3, 3);
-            Console.WriteLine("Editor HTML");
-            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("=============");
+            Console.SetCursorPosition(3, 5);
+            Console.WriteLine("Selecione uma opção abaixo:");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 10);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(14, 14);
+
 
 
         }
